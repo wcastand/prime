0f7bc300b7619ae755b120c8af3f99e66082418b
William Castandet <wcastand@gmail.com>
2018-10-27 21:06:28 +0200

    add generation of primes with atkin algo

--- index.test.js
+++ index.test.js
@@ -1,7 +1,47 @@
-import {generatePrime} from './src/utils'
+import { generatePrime } from './src/utils'
 
-test('expect to generate primes', () => {
-  const res = generatePrime(3)
-  const expected = [2, 3, 5]
+test('expect to generate primes with n < 6', () => {
+  const res1 = generatePrime(1)
+  const res2 = generatePrime(2)
+  const res3 = generatePrime(3)
+  const res4 = generatePrime(4)
+  const res5 = generatePrime(5)
+
+  const expected1 = [2]
+  const expected2 = [2, 3]
+  const expected3 = [2, 3, 5]
+  const expected4 = [2, 3, 5, 7]
+  const expected5 = [2, 3, 5, 7, 11]
+
+  expect(res1).toEqual(expected1)
+  expect(res2).toEqual(expected2)
+  expect(res3).toEqual(expected3)
+  expect(res4).toEqual(expected4)
+  expect(res5).toEqual(expected5)
+})
+
+test('expect to generate primes with n = 6', () => {
+  const res = generatePrime(6)
+  const expected = [2, 3, 5, 7, 11, 13]
   expect(res).toEqual(expected)
+})
+
+test('expect to generate 100 primes', () => {
+  const res = generatePrime(100)
+  expect(res.length).toBe(100)
+})
+
+test('expect to generate 110 primes', () => {
+  const res = generatePrime(110)
+  expect(res.length).toBe(110)
+})
+
+test('expect to generate 10 000 primes', () => {
+  const res = generatePrime(10000)
+  expect(res.length).toBe(10000)
+})
+
+test('expect to generate 20 000 primes', () => {
+  const res = generatePrime(20000)
+  expect(res.length).toBe(20000)
 })
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,57 @@
+// estimate limit to find n prime
+// https://en.wikipedia.org/wiki/Prime_number_theorem#Approximations_for_the_nth_prime_number
+function estimateLimit(n) {
+  return n >= 6 ? parseInt(n * (Math.log(n) + Math.log(Math.log(n)))) : null
+}
+
+// sieve of Atkin algo
+// https://www.geeksforgeeks.org/sieve-of-atkin/
+// https://en.wikipedia.org/wiki/Generating_primes
+/* Mark siev[n] is true if one
+      of the following is true:
+  a) n = (4*x*x)+(y*y) has odd number of
+      solutions, i.e., there exist
+      odd number of distinct pairs (x, y)
+      that satisfy the equation and
+      n % 12 = 1 or n % 12 = 5.
+  b) n = (3*x*x)+(y*y) has odd number of
+      solutions and n % 12 = 7
+  c) n = (3*x*x)-(y*y) has odd number of
+      solutions, x > y and n % 12 = 11
+*/
+function atkin(limit) {
+  const sieve = new Array(limit).fill(false)
+
+  for (let x = 0; x * x < limit; x++) {
+    for (let y = 0; y * y < limit; y++) {
+      let n = 4 * x * x + y * y
+      if (n <= limit && (n % 12 == 1 || n % 12 == 5)) sieve[n] = true
+
+      n = 3 * x * x + y * y
+      if (n <= limit && n % 12 == 7) sieve[n] = true
+
+      n = 3 * x * x - y * y
+      if (x > y && n <= limit && n % 12 == 11) sieve[n] = true
+    }
+  }
+  for (let r = 5; r * r < limit; r++) {
+    if (sieve[r]) for (let i = r * r; i < limit; i += r * r) sieve[i] = false
+  }
+
+  const res = [2, 3]
+  for (let a = 5; a < limit; a++) {
+    if (sieve[a]) res.push(a)
+  }
+  return res
+}
+
 export function generatePrime(n) {
-  return [2, 3, 5]
+  const limit = estimateLimit(n)
+  if (limit === null) {
+    const basePrime = [2, 3, 5, 7, 11]
+    return basePrime.splice(0, n)
+  }
+  const res = atkin(limit)
+  console.log(limit, n, res.length)
+  return res.splice(0, n)
 }
